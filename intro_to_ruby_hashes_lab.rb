33e343e6b48d95eec9862763748633b27c7eafcd
Alex Eisenhauer <eisea904@newschool.edu>
2020-07-19 18:27:32 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -11,11 +11,11 @@ def monopoly_with_second_tier
   # complexity.
   monopoly = {railroads: {}}
   monopoly[:railroads] = {
-    :pieces => 4, 
+    pieces: 4,
     rent_in_dollars: {},
     names: {}
     }
-  monopoly[:railroads][0]
+  monopoly[:railroads]
 end
 
 def monopoly_with_third_tier
